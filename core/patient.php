318a9b689a9e27e84e26fe5cf3a6cd53a1307352
Carlos Rios <carios1usb@gmail.com>
2017-06-01 00:50:07 -0500

    Sql to get data

--- core/patient.php
+++ core/patient.php
@@ -14,10 +14,40 @@ class patient extends base
         parent::__construct();
     }
 
+    /*
+     * SQL to get all patients
+     * @return <array>
+     */
     public function list_all()
     {
         $result_array = array();
         $result = $this->db->query('select * from patients');
+
+        return parent::result_array($result);
+    }
+
+    /*
+     * Get patients by specific age
+     * @param {int} $age
+     * @return <array>
+     */
+    public function filterby($field,$age)
+    {
+        $result_array = array();
+        $result = $this->db->query('select * from patients where '.$field.' > '.$age.';');
+
+        return parent::result_array($result);
+    }
+
+
+    /*
+     * SQL to get count by age
+     * @return <array>
+     */
+    public function groupByAge()
+    {
+        $result_array = array();
+        $result = $this->db->query('SELECT patient_age,COUNT(patient_age) as quantity FROM patients Group BY patient_age');
 
         return parent::result_array($result);
     }
