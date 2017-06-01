c2a45d808f08209d796ea1ad72f7e3f58893efe9
Carlos Rios <carios1usb@gmail.com>
2017-06-01 01:03:42 -0500

    Functions in controller to get data across the model class

--- models/my_patient.php
+++ models/my_patient.php
@@ -6,4 +6,42 @@ include './core/patient.php';
 class my_patient extends patient
 {
 
+	public function __construct()
+    {
+        parent::__construct();
+    }
+
+
+    /*
+     * Get all patients
+     * @return <array>
+     */
+    public function getAllPatient()
+    {
+       return $this->list_all();
+    }
+
+
+    /*
+     * Get patients by age
+     * @param {int} $age
+     * @return <array>
+     */
+    public function getPatientByAge($age){
+    	if ($age) {
+    		$value = array('list' => $this->filterby("patient_age",$age), 'isList'=> true);
+    	}else{
+			$value = array('list' => $this->filterby("patient_age",$age), 'isList'=> false);
+    	}
+    	return $value;
+    }
+
+    /*
+     * Get count by age
+     * @return <array>
+     */
+    public function getCountByAge(){
+    	return $this->groupByAge();
+    }
+	
 }
